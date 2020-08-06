51990812fa1d3d5c588f02250326325852513dd4
macintoshhd <khoinguyenvk9@gmail.com>
2020-08-06 11:03:08 +0700

    Initial Commit

---
+++ AppDocTinTuc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppDocTinTuc
+//
+//  Created by Khôi Nguyễn on 8/6/20.
+//  Copyright © 2020 ProjectiOs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
